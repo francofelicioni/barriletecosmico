f72726cf42c014a76a416946fbed61a0b97b1731
Fran Felicioni <ffelicioni@rica.design>
2024-04-16 01:38:21 +0200

    Refactor productsFactory function to accept a parameter for the number of products to generate

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,17 +2,17 @@ import { faker } from '@faker-js/faker';
 
 const products = [];
 
-export const productsFactory = () => {
-    for (let i = 1; i <= 50; i++) {
+export const productsFactory = (q) => {
+    for (let i = 1; i <= q; i++) {
         const product = {
           id: i,
           title: faker.commerce.product(),
           description: faker.commerce.productDescription(),
           price: faker.commerce.price(),
           thumbnail: faker.image.url(),
+          category: faker.commerce.department(),
           code: faker.string.alphanumeric(6),
-          stock: faker.number.int({min: 1, max: 100}),
-          category: faker.commerce.department()
+          stock: faker.number.int({min: 1, max: 100})
         };
       
         products.push(product);
