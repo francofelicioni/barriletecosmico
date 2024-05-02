113e7035276222c897c5c83289288a0a626bc986
Fran Felicioni <ffelicioni@rica.design>
2024-05-02 21:29:25 +0200

    Initialize handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './src/routes/index.js';
+import handlebars from 'express-handlebars';
 
 const port = 8080;
 const ready = console.log(`Server ready on port ${port}`);
@@ -7,6 +8,8 @@ const ready = console.log(`Server ready on port ${port}`);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", handlebars.engine());
 
 app.listen(port, ready);
 app.use('/api', router);
