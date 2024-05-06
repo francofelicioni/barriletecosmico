287674d39e493cb5693ccc93ffc680553c72c447
Fran Felicioni <ffelicioni@rica.design>
2024-05-06 20:26:32 +0200

    Add update post method  to create new carts

--- src/routes/routes.carts.js
+++ src/routes/routes.carts.js
@@ -5,13 +5,19 @@ const router = Router()
 
 
 router.get('/', readAll)
+router.post('/', create)
 router.get('/:id', read);
-router.post('/:cid/product/:pid', create)
+router.post('/:cid/product/:pid', update)
 
 
 async function readAll(_req, res) {
   try {
     const data = await cartManager.getCarts()
+
+    if (data.length === 0) {
+      return res.json({ status: 200, message: 'No carts created' })
+    }
+
     res.status(200).json(data)
   } catch (error) {
     console.log(error)
@@ -36,9 +42,19 @@ async function read(req, res) {
 }
 
 async function create(req, res) {
+  try {    
+    const newCart = await cartManager.addCart();
+    res.status(201).json(newCart)
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function update(req, res) {
   try {
     const {cid, pid} = req.params
-    const cart = await cartManager.addCart(cid, pid)
+    const cart = await cartManager.updateCart(cid, pid)
     res.status(201).json(cart)
 
   } catch (err) {
