ff94a9baee8cd9b5c2f11b8977c263c156aa618f
Fran Felicioni <ffelicioni@rica.design>
2024-05-18 00:26:31 +0200

    Add id key to cartSchema

--- src/dao/models/cart.model.js
+++ src/dao/models/cart.model.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 
 const cartSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+        unique: true,
+    },
     products: {
         type: Array,
         default: [],
