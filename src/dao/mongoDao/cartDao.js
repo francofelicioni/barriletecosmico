dbd9385a76cde9ee6839117359a27e1164874651
Fran Felicioni <ffelicioni@rica.design>
2024-05-26 22:24:05 +0200

    Enhance cart functionality and product management

--- src/dao/mongoDao/cartDao.js
+++ src/dao/mongoDao/cartDao.js
@@ -1,5 +1,6 @@
 import { cartModel } from "../models/cart.model.js";
 import { productModel } from "../models/product.model.js";
+import productDao from "./product.dao.js";
 
 const getCartById = async (id) => {
     const cartFounded = cartModel.findById(id);
@@ -12,18 +13,67 @@ const createCart = async (data) => {
 
 const addProductToCart = async (cid, pid) => {
     const product = await productModel.findById(pid);
-    if (!product) return { product: false }
-
-    await cartModel.findByIdAndUpdate(cid, { $push: { products: product } });
+    if (!product) return { product: false };
 
     const cart = await cartModel.findById(cid);
-    if (!cart) return { cart: false }
+    if (!cart) { return { cart: false } }
 
+    const productFounded = await cartModel.findOneAndUpdate({ _id: cid, "products.product": pid }, { $inc: { "products.$.quantity": 1 } });
+
+    if (!productFounded) {
+        await cartModel.updateOne({ _id: cid }, { $push: { products: { product: pid, quantity: 1 } } });
+    }
+
+    const cartUpdated = await cartModel.findById(cid)
+    return cartUpdated
+}
+const deleteProductFromCart = async (cid, pid) => {
+    const product = await productModel.findById(pid);
+    if (!product) return { product: false };
+
+    const cart = await cartModel.findOneAndUpdate({ _id: cid, "products.product": pid }, { $inc: { "products.$.quantity": -1 } });
+    if (!cart) return { cart: false };
+    const cartUpdate = await cartModel.findById(cid);
+
+    return cartUpdate;
+};
+
+const update = async (cid, data) => {
+    await cartModel.updateOne({ _id: cid }, { $set: { products: [] } })
+    await cartModel.updateOne({ _id: cid }, { $set: { products: data } })
+
+    const cart = await cartModel.findById(cid)
+    return cart
+}
+
+const updateProductQuantityInCart = async (cartId, productId, quantity) => {
+    const productFounded = await productModel.findById(productId)
+    if (!productFounded) return { product: false }
+
+    const cartFounded = await cartModel.findById(cartId)
+    if (!cartFounded) return { cart: false }
+
+    const cartUpdated = await cartModel.findByIdAndUpdate(
+        cartId,
+        { $inc: { "products.$.quantity": quantity } },
+        { new: true }
+    )
+
+    return cartUpdated
+}
+
+const deleteAllProductsInCart = async (cartId) => {
+    const cart = await cartModel.findByIdAndUpdate(cartId, { products: [] }, { new: true });
     return cart;
 }
 
+
 export default {
-    getCartById,
     createCart,
-    addProductToCart
+    getCartById,
+    addProductToCart,
+    update,
+    updateProductQuantityInCart,
+    deleteProductFromCart,
+    deleteAllProductsInCart
 }
