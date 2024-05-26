8bba617ed3e8ae0265ac88f05493cd5961282df3
Fran Felicioni <ffelicioni@rica.design>
2024-05-26 22:24:59 +0200

    Refactor cart router for enhanced CRUD operations

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -2,13 +2,17 @@ import { Router } from "express";
 import cartDao from "../dao/mongoDao/cartDao.js";
 const router = Router()
 
-router.get('/:cid', read);
+router.get('/:cid', readOne);
 router.post('/', create)
-router.post('/:cid/product/:pid', update)
-router.delete('/:cid/product/:pid', destroy)
+router.post('/:cid/product/:pid', updateByOne)
+router.put('/:cid/product/:pid', updateAll)
 
+router.post('/:cid', update)
 
-async function read(req, res) {
+router.delete('/:cid/product/:pid', destroyOne)
+router.delete('/:cid', destroyAll)
+
+async function readOne(req, res) {
   try {
     const { cid } = req.params;
 
@@ -40,28 +44,79 @@ async function create(_req, res) {
   }
 }
 
-async function update(req, res) {
+async function updateByOne(req, res) {
 
-  const { cid, pid } = req.params;
-  const cart = await cartDao.addProductToCart(cid, pid)
-
-  if (cart.product == false) return res.status(404).json({ status: "Error", msg: `No se encontró el producto con el id ${pid}` });
-  if (cart.cart == false) return res.status(404).json({ status: "Error", msg: `No se encontró el carrito con el id ${cid}` });
-
-  res.status(200).json({ status: "success", payload: cart });
-}
-
-async function destroy(req, res) {
   try {
     const { cid, pid } = req.params;
-    const cart = await cartDao.deleteProductFromCart(cid, pid)
+    const cart = await cartDao.addProductToCart(cid, pid)
 
-    if (cart.product === false) return res.status(400).json({status: "Error", msg: `Product with id ${pid} not founded`})
+    if (cart.product == false) return res.status(404).json({ status: "Error", msg: `No se encontró el producto con el id ${pid}` });
+    if (cart.cart == false) return res.status(404).json({ status: "Error", msg: `No se encontró el carrito con el id ${cid}` });
 
     res.status(200).json({ status: "success", payload: cart });
   } catch (error) {
     return res.status(500).json({ status: "Error", message: "500 Internal Server Error" });
   }
 }
+
+async function updateAll(req, res) {
+
+  try {
+    const { cid, pid } = req.params
+    const { quantity } = req.body
+
+    const cart = await cartDao.updateProductQuantityInCart(cid, pid, quantity)
+
+    if (cart.product == false) return res.status(404).json({ status: "Error", msg: `No se encontró el producto con el id ${pid}` });
+    if (cart.cart == false) return res.status(404).json({ status: "Error", msg: `No se encontró el carrito con el id ${cid}` });
+
+    res.status(200).json({ status: "success", payload: cart });
+  } catch (error) {
+    return res.status(500).json({ status: "Error", message: "500 Internal Server Error" });
+  }
+}
+
+
+async function update(req, res) {
+  try {
+    const { cid } = req.params;
+    const cart = await cartDao.update(cid, body)
+
+    if (!cart.cart) return res.status(404).json({ status: "Error", msg: `No se encontró el carrito con el id ${cid}` });
+
+    res.status(200).json({ status: "success", payload: cart });
+  } catch (error) {
+    return res.status(500).json({ status: "Error", message: "500 Internal Server Error" });
+  }
+}
+
+async function destroyOne(req, res) {
+  try {
+    const { cid, pid } = req.params;
+    const cart = await cartDao.deleteProductFromCart(cid, pid)
+
+    if (cart.product === false) return res.status(400).json({ status: "Error", msg: `Product with id ${pid} not founded` })
+
+    res.status(200).json({ status: "success", payload: cart });
+  } catch (error) {
+    return res.status(500).json({ status: "Error", message: "500 Internal Server Error" });
+  }
+}
+
+async function destroyAll(req, res) {
+  try {
+    const { cid } = req.params
+
+    const cartFounded = cartDao.deleteAllProductsInCart(cid);
+    if (!cartFounded) return res.status(404).json({ status: "Error", message: `Cart with id ${cid} not founded` })
+
+    res.status(200).json({ status: 'Success', payload: cartFounded })
+
+  } catch (error) {
+    res.status(500).json({ status: 'Error', message: '500 Internal Server Error' })
+  }
+}
+
+
 
 export default router;
