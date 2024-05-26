a3487760aeb3225a6a696f3c60293e1d82c476b3
Fran Felicioni <ffelicioni@rica.design>
2024-05-26 21:36:03 +0200

    Create destroy function and update error messages for read and create

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -5,6 +5,7 @@ const router = Router()
 router.get('/:cid', read);
 router.post('/', create)
 router.post('/:cid/product/:pid', update)
+router.delete('/:cid/product/:pid', destroy)
 
 
 async function read(req, res) {
@@ -19,8 +20,8 @@ async function read(req, res) {
 
     return json.status(404).message(`Cart not founded for id ${id}!`)
 
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    return res.status(500).json({ status: 'Error', message: "500 Internal Server Error" });
   }
 }
 
@@ -35,10 +36,9 @@ async function create(_req, res) {
     throw new Error('Error: no data to create a new resource!');
 
   } catch (err) {
-    console.log(err)
+    return res.status(500).json({ status: 'Error', message: err.message });
   }
 }
-
 
 async function update(req, res) {
 
@@ -49,6 +49,19 @@ async function update(req, res) {
   if (cart.cart == false) return res.status(404).json({ status: "Error", msg: `No se encontró el carrito con el id ${cid}` });
 
   res.status(200).json({ status: "success", payload: cart });
+}
+
+async function destroy(req, res) {
+  try {
+    const { cid, pid } = req.params;
+    const cart = await cartDao.deleteProductFromCart(cid, pid)
+
+    if (cart.product === false) return res.status(400).json({status: "Error", msg: `Product with id ${pid} not founded`})
+
+    res.status(200).json({ status: "success", payload: cart });
+  } catch (error) {
+    return res.status(500).json({ status: "Error", message: "500 Internal Server Error" });
+  }
 }
 
 export default router;
