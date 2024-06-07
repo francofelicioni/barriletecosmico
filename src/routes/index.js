174fc0f7677425af3d4e921dfc176af3d9ac0bd2
Fran Felicioni <ffelicioni@rica.design>
2024-06-07 11:25:58 +0200

    Create sessions router and add to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import productsRouter from './product.routes.js'
 import cartsRouter from './carts.routes.js'
+import sessionsRouter from './session.routes.js'
 
 const router = Router()
 
 router.use('/products', productsRouter)
 router.use('/carts', cartsRouter)
+router.use('/sessions', sessionsRouter)
 
 export default router
