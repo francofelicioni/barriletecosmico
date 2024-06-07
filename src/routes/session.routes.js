4f34ac791f27b663d86a7ec18827a99e2d4b8c77
Fran Felicioni <ffelicioni@rica.design>
2024-06-07 12:14:02 +0200

    Make sure a user cannot be created if already exists

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -10,7 +10,13 @@ router.post('/register', register)
 async function register(req, res) {
     try {
         const userData = req.body
+        
+        const userExists = await userDao.getUserByEmail(userData.email)
+        if (userExists) return res.status(400).json({ status: 'Error', message: 'User already exists' })
+
         const newUser = await userDao.createUser(userData)
+        if (!newUser) return res.status(400).json({ status: 'Error', message: 'User cannot be created' })
+
         return res.status(201).json({ status: 'success', payload: newUser })
     } catch (error) {
         return res.status(500).json({ status:'Error', message: 'Internal Server Error' })
