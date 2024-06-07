1fca3be5d1a664f6e06801f2eb8ffc1b8a0340a8
Fran Felicioni <ffelicioni@rica.design>
2024-06-07 11:54:39 +0200

    Add register endpoint to add user register

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,6 +1,20 @@
 import { Router } from "express";
+import { userModel } from "../dao/models/user.model.js";
+import userDao from "../dao/mongoDao/user.dao.js";
 
-const router = Router
+const router = Router();
 
+router.post('/register', register)
+
+
+async function register(req, res) {
+    try {
+        const userData = req.body
+        const newUser = await userDao.createUser(userData)
+        return res.status(201).json({ status: 'success', payload: newUser })
+    } catch (error) {
+        return res.status(500).json({ status:'Error', message: 'Internal Server Error' })
+    }
+}
 
 export default router
