e31bb10dd99864139a5711bdaacda4a157e22ac0
Fran Felicioni <ffelicioni@rica.design>
2024-06-07 11:25:31 +0200

    Refactored app.js for improved readability and consistency in code formatting

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ import router from './src/routes/index.js';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
 import { connectMongoDB } from './src/config/mongoDB.config.js';
-const dbPassword = process.env.DB_PASSWORD
 const port = 8080;
 const ready = console.log(`Server ready on port ${port}`);
+const dbPassword = process.env.DB_PASSWORD
 const URI = `mongodb+srv://franfelicioni:${dbPassword}@bc-ecommerce.zt5qt94.mongodb.net/e-commerce`
 
 connectMongoDB();
@@ -17,15 +17,16 @@ const sessionStore = MongoStore.create({
 });
 
 app.use(
-    express.json(),
-    express.urlencoded({ extended: true }),
-    session({
-        store: sessionStore,
-        secret: 'secret',
-        resave: true,
-        saveUninitialized: true,
-    }),
+  express.json(),
+  express.urlencoded({ extended: false }),
+  session({
+    store: sessionStore,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 15 * 60 * 1000 },
+  })
 );
 
-app.listen(port, ready);
 app.use('/api', router);
+app.listen(port, ready);
