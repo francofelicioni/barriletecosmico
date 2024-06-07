dfc10b6a452ba4d18442e66a8a28291a8d776ec4
Fran Felicioni <ffelicioni@rica.design>
2024-06-07 13:10:40 +0200

    Add middleware in product routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,11 @@ import { Router } from 'express'
 import productRoutes from './product.routes.js'
 import cartRoutes from './cart.routes.js'
 import sessionRoutes from './session.routes.js'
+import { isLogged } from '../middlewares/isLogged.middleware.js'
 
 const router = Router()
 
-router.use('/products', productRoutes)
+router.use('/products', isLogged, productRoutes)
 router.use('/carts', cartRoutes)
 router.use('/sessions', sessionRoutes)
 
