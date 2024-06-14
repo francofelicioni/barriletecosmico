4f8ef4f1ae5a9f69530a9ca9633c15f96fec2e0e
Fran Felicioni <ffelicioni@rica.design>
2024-06-14 20:52:24 +0200

    feat: add jwt login and current endpoint to test

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import passport from 'passport';
+import userDao from "../dao/mongoDao/user.dao.js";
+import { generateToken, verifyToken } from "../utils/jwt.js";
+import { isValidPassword } from "../utils/passwordHash.js";
 
 const router = Router();
 
@@ -9,7 +12,23 @@ router.get('/google', passport.authenticate('google', {
     scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'],
     session: false,
 }), googleLogin);
+router.post('/jwt', jwtLogin);
 router.get('/logout', logout);
+
+router.get('/current', (req, res) => {
+    try {
+        const token = req.cookies.token;
+
+        const tokenVerified = verifyToken(token);
+
+        if (!tokenVerified) return res.status(200).json({ status: 'success', message: 'Invalid token' })
+        
+        return res.status(200).json({ status: 'success', message: 'User logged in' , payload: tokenVerified })
+
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 async function register(req, res) {
     try {
@@ -22,6 +41,24 @@ async function register(req, res) {
 async function login(req, res) {
     try {
         return res.status(200).json({ status: 'success', message: 'User logged in', payload: req.user })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ status: 'Error', message: 'Internal Server Error' })
+    }
+}
+async function jwtLogin(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        const user = await userDao.getUserByEmail(email);
+
+        if (!user || !isValidPassword(user, password)) return res.status(404).json({ status: 'Error', message: 'Email or password incorrect' });
+
+        const token = generateToken(user);
+
+        res.cookie("token", token, { httpOnly: true });
+
+        return res.status(200).json({ status: 'success', message: 'User logged in', payload: user, token })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ status: 'Error', message: 'Internal Server Error' })
