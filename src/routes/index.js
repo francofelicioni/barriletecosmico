0ecab20e38847be1b7fd57a39adbb7656719fb3a
Franco Felicioni <fran.felicioni@gmail.com>
2024-06-27 08:52:28 +0200

    fix: remove isLogged middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,10 @@ import { Router } from 'express'
 import productRoutes from './product.routes.js'
 import cartRoutes from './cart.routes.js'
 import sessionRoutes from './session.routes.js'
-import { isLogged } from '../middlewares/isLogged.middleware.js'
 
 const router = Router()
 
-router.use('/products', isLogged, productRoutes)
+router.use('/products', productRoutes)
 router.use('/carts', cartRoutes)
 router.use('/sessions', sessionRoutes)
 
