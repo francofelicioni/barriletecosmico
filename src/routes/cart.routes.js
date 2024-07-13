ec9861555e585e3f7632ffb6a45dbbcbbfc75c1d
Franco Felicioni <fran.felicioni@gmail.com>
2024-07-14 01:42:22 +0200

    fix: add passportCall for routes, update cartController import name and route

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { authorization, passportCall } from "../middlewares/passport.middleware.js";
-import cartsControllers from "../controllers/carts.controllers.js";
+import cartController from "../controllers/cart.controller.js";
+
 const router = Router()
 
-router.get('/:cid', passportCall('jwt'), authorization("user"), cartsControllers.readOne);
-router.post('/', authorization("user"), cartsControllers.createCart)
-router.post('/:cid/product/:pid', cartsControllers.updateByOne)
-router.put('/:cid/product/:pid', cartsControllers.updateAll)
-router.post('/:cid', passportCall('jwt'), authorization("user"), cartsControllers.update)
-router.delete('/:cid/product/:pid', passportCall('jwt'), authorization("user"), cartsControllers.destroyOne)
-router.delete('/:cid', passportCall('jwt'), authorization("user"), cartsControllers.destroyAll)
+router.post('/', passportCall("jwt"), authorization("user"), cartController.createCart)
+router.get('/:cid', passportCall('jwt'), authorization("user"), cartController.readOne);
+router.post('/:cid/product/:pid',passportCall("jwt"), cartController.updateByOne)
+router.put('/:cid/product/:pid', passportCall("jwt"), cartController.updateAll)
+router.post('/:cid', passportCall('jwt'), authorization("user"), cartController.update)
+router.delete('/:cid/product/:pid', passportCall('jwt'), authorization("user"), cartController.destroyOne)
+router.delete('/:cid', passportCall('jwt'), authorization("user"), cartController.destroyAll)
 
 export default router;
