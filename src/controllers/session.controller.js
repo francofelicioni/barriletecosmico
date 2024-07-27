cf0f6df0cedddaaa77e8974ac5d304ede6c914ff
Franco Felicioni <fran.felicioni@gmail.com>
2024-07-27 19:01:48 +0200

    fix: manage user information by userResponseDto in current method

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -34,7 +34,8 @@ const googleLogin = async (req, res) => {
 
 const current = async (req, res) => {
     try {
-        return res.status(200).json({ status: 'success', message: 'User logged in', payload: req.user })
+        const user = userResponseDto(req.user);
+        return res.status(200).json({ status: 'success', message: 'User logged in', payload: user })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ status: 'Error', message: 'Internal Server Error' })
