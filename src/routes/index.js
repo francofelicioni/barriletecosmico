243f040d841d157e900c8b4f02b5975637ba6870
Franco Felicioni <fran.felicioni@gmail.com>
2024-08-03 19:45:30 +0200

    feat: create mocking routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,14 @@ import { Router } from 'express'
 import productRoutes from './product.routes.js'
 import cartRoutes from './cart.routes.js'
 import sessionRoutes from './session.routes.js'
+import mockingRoutes from './mocking.routes.js'
 
 const router = Router()
 
 router.use('/products', productRoutes)
 router.use('/carts', cartRoutes)
 router.use('/sessions', sessionRoutes)
+router.use('/mockingproducts', mockingRoutes)
+
 
 export default router
