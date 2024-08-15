3a85725b32f3effcb7cd89db4b5b253933ae5135
Franco Felicioni <fran.felicioni@gmail.com>
2024-08-15 20:06:00 +0200

    refactor

--- src/middlewares/isLogged.middleware.js
+++ src/middlewares/isLogged.middleware.js
@@ -1,10 +1,10 @@
 import { request, response } from "express";
 import customErrors from "../errors/customErrors.js";
 
-export const isLogged = async (req = request, res = response, next) => {
+export const isLogged = async (req = request, _res = response, next) => {
    if (req.session.user) {
       next();
    } else {
-     throw customErrors.unAuthorized(`User not logged in!`);
+      throw customErrors.unAuthorized('User not logged in!');
    }
 }
