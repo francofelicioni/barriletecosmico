58f501f9f6e6b5a352a0eccf9c14bf9b858848e1
Franco Felicioni <fran.felicioni@gmail.com>
2024-08-15 20:04:54 +0200

    fix: move app.js file directory

--- src/app.js
+++ src/app.js
@@ -3,16 +3,16 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
-import envs from './src/config/envConfig.js';
-import { connectMongoDB } from './src/config/mongoDB.config.js';
-import initializePassport from './src/config/passport.config.js';
-import { errorHandler } from './src/errors/errorHandler.js';
-import router from './src/routes/index.js';
-import { logger } from './src/utils/logger.js';
+import envConfig from './config/envConfig.js';
+import { connectMongoDB } from './config/mongoDB.config.js';
+import initializePassport from './config/passport.config.js';
+import router from './routes/index.js';
+import { errorHandler } from './errors/errorHandler.js';
+import { logger } from './utils/logger.js';
 
-const port = envs.PORT;
+const port = envConfig.PORT;
 const ready = console.log(`Server ready on port ${port}`);
-const dbPassword = envs.DB_PASSWORD
+const dbPassword = envConfig.DB_PASSWORD
 const URI = `mongodb+srv://franfelicioni:${dbPassword}@bc-ecommerce.zt5qt94.mongodb.net/e-commerce`
 
 connectMongoDB();
@@ -28,12 +28,12 @@ app.use(
   express.urlencoded({ extended: false }),
   session({
     store: sessionStore,
-    secret: envs.SESSION_SECRET,
+    secret: envConfig.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
     cookie: { maxAge: 15 * 60 * 1000 },
   }),
-  cookieParser( envs.SESSION_SECRET ),
+  cookieParser( envConfig.SESSION_SECRET ),
   passport.initialize(),
   passport.session()
 );
