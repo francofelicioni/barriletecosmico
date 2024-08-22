31da0964c6c6afdb33e914704fdfb387ac710690
Franco Felicioni <fran.felicioni@gmail.com>
2024-08-22 08:20:12 +0200

    fix: update passportCall and authorization functions

--- src/middlewares/passport.middleware.js
+++ src/middlewares/passport.middleware.js
@@ -5,29 +5,26 @@ import customErrors from "../errors/customErrors.js";
 export const passportCall = (strategy) => {
 
     return async (req = request, res = response, next) => {
-        passport.authenticate(strategy, (err, user, info) => {
-            if (err) {
-                return next(err);
-            }
-            if (!user) {
-               throw customErrors.unAuthorized(info.message);
-            }
+        passport.authenticate(strategy, (error, user, info) => {
+            if (error) return next(error);
+            if (!user) res.status(401).json({ status: "error", msg: info.message ? info.message : info.toString() });
+            
             req.user = user;
-            return next();
+            next();
         })(req, res, next);
     }
 }
 
+export const authorization = (roles) => {
+    return async (req = request, _res = response, next) => {
+        try {
+            if (!req.user) throw customErrors.unAuthorized('User not logged in!');
+            const roleAuthorized = roles.includes(req.user.role);
+            if (!roleAuthorized) throw customErrors.unAuthorized('User not authorized!');
 
-export const authorization = (role) => {
-    return async (req = request, res = response, next) => {
-        if (!req.user) {
-            throw customErrors.unAuthorized('User not logged in!');
+            next();
+        } catch (error) {
+            return next(error);
         }
-
-        if (req.user.role !== role) {
-            throw customErrors.unAuthorized('User not authorized!');
-        }
-        next();
     }
 }
