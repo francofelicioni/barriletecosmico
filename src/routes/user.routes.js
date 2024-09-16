d257199e45b6b0b9018e3f153b42b84587ba5546
Franco Felicioni <fran.felicioni@gmail.com>
2024-09-16 21:47:58 +0200

    feat: add post route to add documents

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
 import userController from "../controllers/user.controller.js";
+import { uploader } from "../utils/fileUploader.js";
+import { authorization, passportCall } from "../middlewares/passport.middleware.js";
 
 const router = Router();
 
 router.post('/email/reset-password', userController.sendResetEmail)
 router.post('/reset-password', userController.resetPassword)
 router.get('/premium/:uid', userController.changeRole)
+router.post(
+    '/:uid/documents',
+    passportCall('jwt'),
+    authorization(['user', 'premium']),
+    uploader.fields([
+        { name: 'profiles', maxCount: 1 },
+        { name: 'products', maxCount: 1 },
+        { name: 'documents', maxCount: 3 },
+    ]),
+    userController.addDocuments
+);
 
 export default router
